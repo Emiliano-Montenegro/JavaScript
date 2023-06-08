6acf559e3876eee6f6b900ba4c25e4f1d1ddcaf5
Emiliano-Montenegro <montenegrol.emiliano@gmail.com>
2023-06-08 15:40:29 -0300

    Simulador interactivo primer pre entrega JS

--- app.js
+++ app.js
@@ -20,19 +20,18 @@ do{
     "2- Shampoo para cabello: $" + PRODUCTO2 + "\n" +
     "3- Aceite para barba: $" + PRODUCTO3 + "\n"));
 
+    cantidad = parseInt(prompt("Ingrese la cantidad que desea"));
+
     switch (opcion) {
-        case 1:
-            cantidad = parseInt(prompt("Ingrese la cantidad que desea"));
+        case 1:            
             total = calculo(total, PRODUCTO1, cantidad);
-            break;
-    
+            break;    
+
         case 2:
-            cantidad = parseInt(prompt("Ingrese la cantidad que desea"));
             total = calculo(total, PRODUCTO2, cantidad);
             break;
-
+            
         case 3:
-            cantidad = parseInt(prompt("Ingrese la cantidad que desea"));
             total = calculo(total, PRODUCTO3, cantidad);
             break;
 
