520b534fca07aedb24dfd125125f576aebc39528
Emiliano-Montenegro <montenegrol.emiliano@gmail.com>
2023-06-09 09:46:26 -0300

    Simulador interactivo primer pre entrega JS

--- app.js
+++ app.js
@@ -12,34 +12,45 @@ function calculo(acumulado, precio, cantidad) {
     return acumulado + precio*cantidad
 }
 
-alert('Bienvenido al carrito de compra de tu Barberia');
-
-do{
-    opcion = parseInt (prompt('Que producto deseas elegir?' + "\n" + "\n" +
+function mostrarMenu() {
+    return parseInt (prompt('Que producto deseas elegir?' + "\n" + "\n" +
     "1- Cera para cabello: $" + PRODUCTO1 + "\n" +
     "2- Shampoo para cabello: $" + PRODUCTO2 + "\n" +
     "3- Aceite para barba: $" + PRODUCTO3 + "\n"));
+}
+
+alert('Bienvenido al carrito de compra de tu Barberia');
+
+do{
+    opcion = mostrarMenu();
+
+    while (!(opcion >= 1 && opcion <=3)) {
+        alert("Ingrese una opción correcta")
+        opcion = mostrarMenu()
+    }
 
     cantidad = parseInt(prompt("Ingrese la cantidad que desea"));
 
     switch (opcion) {
-        case 1:            
+        case 1:
             total = calculo(total, PRODUCTO1, cantidad);
             break;    
-
         case 2:
             total = calculo(total, PRODUCTO2, cantidad);
             break;
-            
         case 3:
             total = calculo(total, PRODUCTO3, cantidad);
             break;
-
         default:
             alert("Ingrese una opcion correcta");
     }
     agregar = prompt("Deseas agregar mas productos al carrito? Si \ No")
+    
+    while (!(agregar.toLowerCase() === "si" || agregar.toLowerCase() === "no")) {
+        alert("Ingrese una opción correcta (Si / No)");
+        agregar = prompt("Deseas agregar más productos al carrito? Si / No");
+    }
+    
 } while (agregar.toLowerCase() !== "no" )
-
 
 alert ("El total a pagar es: $" + total)
